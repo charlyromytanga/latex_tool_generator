68bb0c0de91a33d44767602ab680f44dd872c449
charlyromytanga <charlyromytanga.cytech@gmail.com>
2026-04-06 22:15:44 +0200

    formations et expériences

---
+++ scripts/bootstrap_all.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Automatisation de l'appel des orchestrateurs pour l'insertion des données
+
+set -e
+
+# Appel orchestrateur formations
+PYTHONPATH=src python3 src/orchestration/formations_orchestrator.py --db-path db/recruitment_assistant.db --schema-path db/schema_init.sql --log-level INFO
+
+# Appel orchestrateur expériences
+PYTHONPATH=src python3 src/orchestration/experiences_orchestrator.py --db-path db/recruitment_assistant.db --schema-path db/schema_init.sql --log-level INFO
+
+# Appel orchestrateur projets
+PYTHONPATH=src python3 src/orchestration/projects_orchestrator.py --repo-path . --db-path db/recruitment_assistant.db --schema-path db/schema_init.sql --log-level INFO
