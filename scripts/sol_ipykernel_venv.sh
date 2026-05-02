9a1ac73f6317c8f3a2635c9b8cebe33783f63042
charlyromytanga <charlyromytanga.cytech@gmail.com>
2026-05-02 07:57:59 +0200

    new db

---
+++ scripts/sol_ipykernel_venv.sh
@@ -0,0 +1,3 @@
+/home/cytech/dev/python/latex_tool_generator/.venv/bin/python -m ensurepip --upgrade
+/home/cytech/dev/python/latex_tool_generator/.venv/bin/python -m pip install ipykernel -U --force-reinstall
+pip3 install --upgrade pip
